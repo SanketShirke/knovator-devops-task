e1a872b7046185dc76bb1c85532f78df441b93fb
SanketShirke <95741537+SanketShirke@users.noreply.github.com>
2025-08-18 20:44:25 +0530

    Delete knovator-devops-task/step2 directory

--- knovator-devops-task/step2/deploy/scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-mkdir -p env nginx certs scripts
-docker compose -f docker-compose.prod.yml pull
-docker compose -f docker-compose.prod.yml up -d --remove-orphans
-docker image prune -f
